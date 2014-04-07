b430d1d16dca5dfe6c5ef6a56544f6b778cbcdfc
Brian <bpsc222@g.uky.edu>
2014-04-07 11:59:20 -0400

    Added plenty of scanner things. Array of structs that contain token and type of each individual element of command

--- envsh.c
+++ envsh.c
@@ -5,41 +5,87 @@
 
 # define MAXLINE 256
 
-int scanner(char* command, char* token, char* type, int pos);
+typedef struct {
+	char* token;
+	char* type;
+} Element;
+
+int changed_prompt = 0;
+char prompt[MAXLINE];
+
+void scanner(char* command, Element* each_element);
 void parser();
 
-int main() {
+int main(int argc, char *argv[]) {
 
+//quiet mode is not working at the moment. I can't get it to work
+	int quiet = 0;
+	if (argv[1] == "quiet") {
+		quiet = 1;
+	}
 	while(1) {
-		printf("envsh> ");
+		if (quiet == 0) {
+			if (changed_prompt == 0) {
+				printf("envsh> ");
+			} else {
+			}
+		}
 		parser();
 	}
 	return 0;
 }
 
-int scanner(char* command, char* token, char* type, int pos) {
+void scanner(char* command, Element* each_element) {
 
-	for (; command[pos] != ' '; pos++) {
-		char character = command[pos];
-		if ((character == '%') || (character == '>') || (character == '<')) {
-			type = "metachar";
+	int current = 0; // current place in element array 
+	int i; // index used for command line 
+	for (i = 0; i < MAXLINE; ++i) {
+		if ((command[i] == '%') && (i == 0)) {
+			int index = 0;
+			char comment[MAXLINE];
+			while (command[i] != '\n') {
+				comment[index] = command[i];
+				++i;
+				++index;
+			}
+			each_element[current].type = "metachar";
+			each_element[current].token = comment;
+		} else if (command[i] == '<') {
+
+                        ++current;
+                        each_element[current].type = "metachar";
+                        each_element[current].token = "<";
+		} else if (command[i] == '>') {
+
+			++current;
+			each_element[current].type = "metachar";
+			each_element[current].token = ">";
+		} else if (command[i] == ' ') {
+
+		} else if (command[i] == '"') { 
+			while (command[i] != '"') {
+				
+				++i;
+			}
+			each_element[current].type = "string";
+		} else {
+
 		}
 	}
-
-	return pos;
 }
 
 void parser() {
 
-	char command[MAXLINE];
-	char token[MAXLINE];
-	char type[MAXLINE];
+	char command[MAXLINE]; // command line input
+	Element each_element[MAXLINE]; // array of tokens/type of each 
+		// command line input
+	int cur_element = 0; // current index of token from command line
 
 	fgets(command, MAXLINE, stdin);
 	if (feof(stdin))
 		exit(0);
-	int position = scanner(command, token, type, 0);
-	while(token != '\0') {
-		position = scanner(command, token, type, position);
+	scanner(command, each_element);
+	while(each_element[cur_element].token != '\0') {
+		scanner(command, each_element);
 	}
 }
