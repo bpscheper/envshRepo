d8a7ba20c50de9a66227b78f71c1e931ce65dc95
bpscheper <bpsc222@g.uky.edu>
2014-04-10 14:17:56 -0400

    Update envsh.c
    Removed the code for built in commands. Next commit will be the addition of the proper way to write it

--- envsh.c
+++ envsh.c
@@ -46,70 +46,21 @@ void scanner(char command[], Element* each_element) {
 	int i; // index used for command line 
 	for (i = 0; i < MAXLINE; ++i) {
 		//First line is a special case, check for built-in commands
-		if (i ==0){
-			//Check for a comment
-			if ((command[i] == '%')) {
-				int index = 0;
-				char comment[MAXLINE];
-				while (command[i] != '\n') {
-					comment[index] = command[i];
-					++i;
-					++index;
-				}
-				each_element[current].type = "metachar";
-				strncpy(each_element[current].token, comment, sizeof(comment)/sizeof(*comment));
-				return;
+		//Check for a comment
+		if ((command[i] == '%')) {
+			int index = 0;
+			char comment[MAXLINE];
+			while (command[i] != '\n') {
+				comment[index] = command[i];
+				++i;
+				++index;
 			}
-			else {
-				//Check for built in commands
-				char firstcmd[6];
-				strncpy(firstcmd, command, 6);
-				if (strcmp(firstcmd, "prompt") == 0){
-					each_element[current].type = "prompt";
-					strcpy(each_element[current].token, "prompt");	
-					++current;
-				} else if (strcmp(firstcmd, "setdir") == 0) {
-					each_element[current].type = "setdir";
-					strcpy(each_element[current].token, "setdir");	
-					++current;
-					continue;
-				} else if (strcmp(firstcmd, "setenv") == 0) {
-					each_element[current].type = "setenv";
-					strcpy(each_element[current].token, "setenv");	
-					++current;
-					continue;
-				}
-
-				char check_unset[8];
-				strncpy(check_unset, command, 8);
-				if (strcmp(check_unset, "unsetenv") == 0) {
-					each_element[current].type = "unsetenv";
-					strcpy(each_element[current].token, "unsetenv");	
-					++current;
-					continue;
-				}
-
-				char list_env[7];
-				strncpy(list_env, command, 7);
-				if (strcmp(list_env, "listenv") == 0) {
-                    each_element[current].type = "listenv";
-					strcpy(each_element[current].token, "listenv");	
-                    ++current;
-					continue;
-				}
-
-				char bye[3];
-                strncpy(bye, command, 3);
-                if (strcmp(bye, "bye") == 0) {
-                	each_element[current].type = "bye";
-					strcpy(each_element[current].token, "bye");	
-                    ++current;
-                    continue;
-                }
-			}
+			each_element[current].type = "metachar";
+			strncpy(each_element[current].token, comment, sizeof(comment)/sizeof(*comment));
+			return;
 		} else if (command[i] == '<') {
 			++current;
-           	each_element[current].type = "metachar";
+           		each_element[current].type = "metachar";
 			strcpy(each_element[current].token, "<");
 			continue;
 		} else if (command[i] == '>') {
@@ -125,14 +76,14 @@ void scanner(char command[], Element* each_element) {
 				while(command[i] != ' '){
 					str[len] = command[i];
 					str[len + 1] = '\0';
-	                ++len;
-	                ++i;
+	                		++len;
+	                		++i;
 				}
 
 				each_element[current].type = "stringliteral";
-	            strcpy(each_element[current].token, str);
+	            		strcpy(each_element[current].token, str);
 
-    	        ++current;
+    	        		++current;
 			}
 		} else if (command[i] == '"') { 
 			++i;
